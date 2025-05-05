711dd90feab0e15bcf7333367230363726e9060a
AlesiaMi <alesiamikevich@gmail.com>
2025-05-05 10:30:07 +0300

    edit

--- WEB/WEB/obj/Debug/net8.0/WEB.AssemblyInfo.cs
+++ WEB/WEB/obj/Debug/net8.0/WEB.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444b874edd762472d4d4e26e6eecc9705a6e745c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f72353b3cd51b939b2a53c3aca41d2cbcc1a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
